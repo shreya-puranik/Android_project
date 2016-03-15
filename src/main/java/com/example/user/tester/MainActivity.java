307258b742602e6b637e74013811dfdc33b8597a
shreya-puranik <shreyapuranik5@gmail.com>
2016-03-15 19:57:24 +0530

    Added files via upload

---
+++ src/main/java/com/example/user/tester/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.user.tester;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               /* Fragment fragment = null;
+                if (view == findViewById(R.id.button)) {
+                    fragment = new BlankFragment();
+
+                    FragmentManager manager = getSupportFragmentManager();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    transaction.replace(R.id.fragment, fragment);
+                    transaction.commit();*/
+                Intent i = new Intent(MainActivity.this, Start_activity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ src/main/java/com/example/user/tester/Start_activity.java
@@ -0,0 +1,15 @@
+package com.example.user.tester;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+    public class Start_activity extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fragment);
+        }
+    }
+
+
+
